6259b070bfcda7da474b45e9a473b35f28b156ed
Mccona <65399425+Mccona@users.noreply.github.com>
2025-03-17 22:22:56 +0000

    Configure OpenCV environment
    To test whether OpenCV is functional under MacOS environment

--- FacialRecog/Main.cpp
+++ FacialRecog/Main.cpp
@@ -1,6 +1,15 @@
-//
-// Created by Zihan Xu on 25-3-5.
-//
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+int main() {
+    Mat srcImage = imread("../TestFiles/Test.png");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
 
-
-balabalabalabla
+        return -1;
+    }
+    imshow("image", srcImage);
+    waitKey(0);
+    return 0;
+}
