2fcd8b2a45f0a011e7bf6c8183f2f19035742664
yquan0000 <qyuan858@gmail.com>
2025-04-21 23:28:46 +0100

    Update main.jsx

--- lib/web/src/main.jsx
+++ lib/web/src/main.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // 确保引入 index.css
+import './index.css'; // Import global styles
 
-// 确保 HTML 里存在 id="root" 的元素
+// Get the root DOM element where the React app will be mounted
 const rootElement = document.getElementById('root');
 
+// Throw an error if root element is not found
 if (!rootElement) {
-  throw new Error("No root element found! Make sure you have <div id='root'></div> in index.html");
+  throw new Error("Root element not found! Please ensure your HTML has a <div id='root'></div> element");
 }
 
-// 渲染 React 应用
+// Create a root and render the application in Strict Mode
 createRoot(rootElement).render(
   <StrictMode>
     <App />
