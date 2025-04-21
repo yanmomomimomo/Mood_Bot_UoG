f868127cf426dd524884ddcc7302aa341219700f
yquan0000 <qyuan858@gmail.com>
2025-04-21 23:11:07 +0100

    Add files via upload

---
+++ lib/Face/faces.hpp
@@ -0,0 +1,14 @@
+#ifndef __FACES_HPP
+#define __FACES_HPP
+
+#include <cstdint>
+
+extern const uint16_t angry_face[172 * 320];
+extern const uint16_t think_face[172 * 320];
+extern const uint16_t sleep_face[172 * 320];
+extern const uint16_t normal_face[172 * 320];
+extern const uint16_t happy_face[172 * 320];
+extern const uint16_t sad_face[172 * 320];
+extern const uint16_t singing_face[172 * 320];
+extern const uint16_t comfort_face[172 * 320];
+#endif // ANGRY_FACE_HPP
